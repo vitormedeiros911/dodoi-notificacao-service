4b6896b0883e3dbc985b1c4f6f6d6b72a4475e1c
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-12-02 11:03:58 -0300

    chore: Adiciona tratamento de erro

--- src/push-notification/push-notification.service.ts
+++ src/push-notification/push-notification.service.ts
@@ -9,44 +9,45 @@ export class PushNotificationService {
 
   private readonly appId = process.env.PUSH_NOTIFICATION_APP_ID;
   private readonly apiKey = process.env.PUSH_NOTIFICATION_API_KEY;
-  private readonly apiUrl = 'https://api.onesignal.com/notifications';
+  private readonly apiUrl = process.env.PUSH_NOTIFICATION_API_URL;
 
   async sendNotification(data: NotificacaoDto) {
-    console.log(this.apiKey);
-
-    const response = await firstValueFrom(
-      this.httpService
-        .post(
-          this.apiUrl,
-          {
-            app_id: this.appId,
-            headings: { en: data.titulo, pt: data.titulo },
-            contents: { en: data.mensagem, pt: data.mensagem },
-            target_channel: 'push',
-            // filters: [
-            //   {
-            //     field: 'tag',
-            //     key: data.tagKey,
-            //     relation: '=',
-            //     value: data.tagValue,
-            //   },
-            // ],
-          },
-          {
-            headers: {
-              Authorization: `Key ${this.apiKey}`,
-              'Content-Type': 'application/json',
+    try {
+      await firstValueFrom(
+        this.httpService
+          .post(
+            this.apiUrl,
+            {
+              app_id: this.appId,
+              headings: { en: data.titulo, pt: data.titulo },
+              contents: { en: data.mensagem, pt: data.mensagem },
+              target_channel: 'push',
+              filters: [
+                {
+                  field: 'tag',
+                  key: data.tagKey,
+                  relation: '=',
+                  value: data.tagValue,
+                },
+              ],
             },
-          },
-        )
-        .pipe(
-          catchError((error) => {
-            console.log('error', error);
-            return error;
-          }),
-        ),
-    );
-
-    console.log(response);
+            {
+              headers: {
+                Authorization: `Key ${this.apiKey}`,
+                'Content-Type': 'application/json',
+              },
+            },
+          )
+          .pipe(
+            catchError((error) => {
+              console.log('error', error);
+              return error;
+            }),
+          ),
+      );
+    } catch (error) {
+      console.log('error', error);
+      throw error;
+    }
   }
 }
